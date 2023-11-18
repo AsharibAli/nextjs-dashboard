8502edb27c54ef7f92ab77b31bf0fcd8c5066b0d
Asharib Ali <asharibarain4@gmail.com>
2023-11-18 13:56:33 +0500

    update: fix the error

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -11,11 +11,11 @@ export default async function InvoicesTable({
   query: string;
   currentPage: number;
 }) {
-  const invoices = await fetchFilteredInvoices(query, currentPage);
+  const invoice = await fetchFilteredInvoices(query, currentPage);
   // ...
   <td className="flex justify-end gap-2 whitespace-nowrap px-6 py-4 text-sm">
-    <UpdateInvoice id={invoices.id} />
-    <DeleteInvoice id={invoices.id} />
+    <UpdateInvoice id={invoice.id} />
+    <DeleteInvoice id={invoice.id} />
   </td>;
   // ...
 
