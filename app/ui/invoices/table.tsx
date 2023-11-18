e5e460c91d9935497ac79d06bbbcad1dfb41b91e
Asharib Ali <asharibarain4@gmail.com>
2023-11-18 13:50:26 +0500

    update: mutating data, handling errors, accessibility.

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
-import InvoiceStatus from '@/app/ui/invoices/status';
-import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
-import { fetchFilteredInvoices } from '@/app/lib/data';
+import Image from "next/image";
+import { UpdateInvoice, DeleteInvoice } from "@/app/ui/invoices/buttons";
+import InvoiceStatus from "@/app/ui/invoices/status";
+import { formatDateToLocal, formatCurrency } from "@/app/lib/utils";
+import { fetchFilteredInvoices } from "@/app/lib/data";
 
 export default async function InvoicesTable({
   query,
@@ -12,6 +12,12 @@ export default async function InvoicesTable({
   currentPage: number;
 }) {
   const invoices = await fetchFilteredInvoices(query, currentPage);
+  // ...
+  <td className="flex justify-end gap-2 whitespace-nowrap px-6 py-4 text-sm">
+    <UpdateInvoice id={invoices.id} />
+    <DeleteInvoice id={invoices.id} />
+  </td>;
+  // ...
 
   return (
     <div className="mt-6 flow-root">
